1775aaff730dd6310e7b601380a0af75d37d6f94
eribrin <eribrinrin@gmail.com>
2024-03-06 12:10:40 -0500

    set up index.ts to include routing information

--- Final-Project/client/src/router/index.ts
+++ Final-Project/client/src/router/index.ts
@@ -1,20 +1,36 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePage from '../views/HomePage.vue'
+import MyActivity from '../views/MyActivity.vue'
+import FriendActivity from '../views/FriendActivity.vue'
+import AdminPage from '../views/AdminPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  // }
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/my-activity',
+    name: 'MyActivity',
+    
+    // lazy loaded
+    component: () => import(/* webpackChunkName: "about" */ '../views/MyActivity.vue')
+  },
+  {
+    path: '/friend-activity',
+    name: 'FriendActivity',
+    
+    // lazy loaded
+    component: () => import(/* webpackChunkName: "about" */ '../views/FriendActivity.vue')
+  },
+  {
+    path: '/admin',
+    name: 'AdminPage',
+    
+    // lazy loaded
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminPage.vue')
+  }
 ]
 
 const router = createRouter({
