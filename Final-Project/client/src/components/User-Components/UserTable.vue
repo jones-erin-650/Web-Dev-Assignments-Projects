1ccc72fe465e53aabf37d6658619069567137797
eribrin <eribrinrin@gmail.com>
2024-03-11 12:08:41 -0400

    created a template for the user table and imported it into the users.vue page (doesn't work for some reason)

---
+++ Final-Project/client/src/components/User-Components/UserTable.vue
@@ -0,0 +1,29 @@
+<script>
+  // import User from './User'
+
+  export default {
+      name: 'UserTable',
+      // props: {
+      //     user: Array
+      // }
+  }
+</script>
+
+<!-- should be a table of all the registered users with the variable at the top then each row being the user's variable -->
+<template>
+  <table class="table">
+  <!-- top row with the names of the columns -->
+    <thead>
+      <tr>
+        <th>PFP</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Email</th>
+        <th>User Name</th>
+        <th>ID</th>
+        <th>isAdmin</th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
--- Final-Project/client/src/pages/Users.vue
+++ Final-Project/client/src/pages/Users.vue
@@ -1,7 +1,21 @@
-<template>
-  <h3>User Information</h3>
-</template>
-
 <script>
+  import { ref } from 'vue';  
+  import UserTable from '../components/User-Components/UserTable.vue'
 
 </script>
+
+<template>
+  <div class="container">
+    <div class="columns">
+      <!-- spacing column -->
+      <div class="column"></div>
+      <!-- main part with the table -->
+      <div class="column is-11">
+        <UserTable />
+      </div>
+      <!-- spacing column -->
+      <div class="column"></div>
+    </div>
+  </div>
+</template>
+
