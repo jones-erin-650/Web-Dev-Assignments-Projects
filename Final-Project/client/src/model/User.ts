bd75f8b2302830f98611da2077c3b0b86205978d
eribrin <eribrinrin@gmail.com>
2024-03-11 11:29:22 -0400

    created an interface for workout data

--- Final-Project/client/src/model/User.ts
+++ Final-Project/client/src/model/User.ts
@@ -1,10 +1,15 @@
 import data from "../data/users.json";
 
 export interface User {
-    id: number,
-    firstName: string,
-    userName: string,
-    isAdmin: boolean,
-    userWorkouts: []
-  }
+  id: number,
+  firstName: string,
+  lastName: string,
+  userName: string,
+  isAdmin: boolean,
+  profilePicture: string,
+  userWorkouts: []
+}
   
+export function getUsers(): User[] {
+  return data.items;
+}
