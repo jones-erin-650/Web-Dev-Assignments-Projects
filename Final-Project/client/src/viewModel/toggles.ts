ff9fe3e93cb7f957c8a8a0274c47986e4f4d0326
eribrin <eribrinrin@gmail.com>
2024-03-14 11:36:34 -0400

    created a ts file that holds a ref variable for a modal toggle so multiple components can use it

---
+++ Final-Project/client/src/viewModel/toggles.ts
@@ -0,0 +1,11 @@
+import { ref } from "vue";
+
+// lots of components have to toggle the edit modal on and off, this file is just a way to have reactive elements for those toggles that can be imported from wherever
+// the only reason this is seperated while something like the navbar or login dropdown toggle isn't is because this is used by multiple components
+
+const modalIsActive = ref(false)
+// returns the variable so other files can access it
+export const refModalIsActive = () =>  modalIsActive
+export const toggleModal = () => {
+  modalIsActive.value = !modalIsActive.value
+}
