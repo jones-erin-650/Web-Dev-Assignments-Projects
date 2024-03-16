a24334aa76da28ca90fc73170099a80d045cec3d
eribrin <eribrinrin@gmail.com>
2024-03-15 21:25:09 -0400

    tried making friend activity, all the posts show up but their handles and user information are messeg up

--- Final-Project/client/src/pages/FriendActivity.vue
+++ Final-Project/client/src/pages/FriendActivity.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import { getUserWorkouts, type User, getUsers } from '@/model/User'
+  
+
+  //imports user array
+  const users = ref([] as User[]) 
+  users.value = getUsers()
+
+  // components
+  import WorkoutPost from '@/components/Workout-Components/WorkoutPost.vue';
 </script>
 
 <template>
-  
+
   <div>
-    <h3>friend activity</h3>
+    
+    <div v-for="user in users" :key="user.id">
+      <WorkoutPost v-for="workout in user.userWorkouts" :key="workout.postId"
+        :user="user"
+        :workout="workout"
+      />
+    </div>
   </div>
+
 </template>
+
 
