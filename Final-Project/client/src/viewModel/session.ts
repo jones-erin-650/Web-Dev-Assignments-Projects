bb27944326e3404f14278b2a010cdf8bbfbb53d1
eribrin <joneserin650@gmail.com>
2024-04-30 15:52:39 -0400

    changed a lot in the session.ts relating to the current user and login stuff, probably broke a lot because of it

--- Final-Project/client/src/viewModel/session.ts
+++ Final-Project/client/src/viewModel/session.ts
@@ -1,28 +1,51 @@
 // this will be imported to get the current user
-import { ref } from "vue";
 import type { User } from "@/model/User";
-import type { Activity } from "@/model/Activity";
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+import * as myFetch from "@/model/myFetch";
+
 
 // currentUser is declared here
 
-const currentUser = ref()
+const session  = reactive({
+    user: null as User | null,
+    isLoading: 0,
+});
 
-// undefined by default
-console.log("If there's no user logged in this is undefined: " + currentUser.value)
 
 // in oder to use curretUser in other files you need to import this function that returns the variable
 
-export const refCurrentUser = () =>  currentUser
+export const refSession = () =>  session
 
 
-export const setRefCurrentUser = (input: User) => {
-    currentUser.value = input
-    console.log("currentUser in currentuser.ts " + currentUser)
-    console.log("value: " + currentUser.value)
-    console.log("name: " + currentUser.value.firstName)
+export function useLogin() {
+    const router = useRouter();
+    return {
+        async login(user: User) {
+            const x = await api<User>("users/login", user);
+            if(x){
+                session.user = x.data;
+                router.push("/");
+            }
+        },
+        logout() {
+            session.user = null;
+            router.push("/login");
+        }
+    };
 }
 
-export const logOut = () => {
-    currentUser.value = undefined
+export function api<T>(action: string, data?: unknown, method?: string){
+    session.isLoading++;
+    return myFetch.api<T>(action, data, method)
+    .then(x=>{
+        if(!x.isSuccess){
+            // showError(x);
+            console.log("error: " + x);
+        }
+        return x;
+    })
+    // should show an error on the catch
+    .catch()
+    .finally(() => session.isLoading--);
 }
-export const getCurrentUserActivities = () => currentUser.value.userActivities
