423fe6bb8e8d193a2bd724f1d18838c466e7d2e0
joneserin650 <joneserin650@gmail.com>
2024-05-08 14:47:27 -0400

    added functions to add and edit users

--- Final-Project/client/src/model/User.ts
+++ Final-Project/client/src/model/User.ts
@@ -1,5 +1,6 @@
 import userData from "../../../server/data/users.json";
 import activityData from "../../../server/data/activities.json";
+import { ref } from "vue";
 
 export interface User {
   id: number,
@@ -10,6 +11,29 @@ export interface User {
   profilePicture: string,
   isAdmin: boolean,
   
+}
+
+// this is just a simple way to get an empty user template to add and edit new users
+export const newUser = ref()
+// to import to other files
+export const refNewUser = () => newUser
+// 
+export const setRefNewUser = (input: User ) => {
+  newUser.value = input
+}
+
+export const setEmptyUser = () => {
+  const activityToSetNewUser: User = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    handle: '',
+    email: '',
+    profilePicture: '',
+    isAdmin: false,
+  }
+  // replaces the current newActivity value with the empty one
+  setRefNewUser(activityToSetNewUser)
 }
   
 // gets the userData out of the user.json file and returns an array of users
@@ -33,3 +57,21 @@ export function getUserFromHandle(handle: String) {
 const handleTest = getUserFromHandle("@eribrin")
 
 console.log("getUserFromHandle test: " + handleTest?.firstName);
+
+export function addUser(input: User){
+  const last = userData.items[userData.items.length - 1];
+  input.id = last.id+1
+  userData.items.push(input)
+  setEmptyUser()
+}
+
+export const editUser = (originalUser: User, newUser: User) => {
+    // the original Id needs to be preserved 
+    const originalID = originalUser.id
+
+    // replaces the value at that index with the new activity
+    userData.items[originalID-1] = newUser
+    // preserve the original id
+    userData.items[originalID-1].id = originalID
+    
+  }
