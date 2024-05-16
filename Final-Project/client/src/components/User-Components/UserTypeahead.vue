01104c0cc23a276d5fa70b4d892de30c6423d84a
joneserin650 <joneserin650@gmail.com>
2024-05-16 13:42:04 -0400

    imported oruga

--- Final-Project/client/src/components/User-Components/UserTypeahead.vue
+++ Final-Project/client/src/components/User-Components/UserTypeahead.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+
+
   
 // prop functions
   function itemProjection() {
@@ -8,6 +10,7 @@
 
   }
   function onInputEventHandler() {
+    // on input we need to send a request to the server  to get the new filtered data
 
   }
   function onFocusEventHandler() {
@@ -20,16 +23,24 @@
 </script>
 
 <template>
-    <vue3-simple-typeahead
-        id="typeahead_id"
-        placeholder="Search User Activities"
-        :items="['One','Two','Three']"
-        :minInputLength="1"
-        :itemProjection="itemProjection()"
-        @selectItem="selectItemEventHandler"
-        @onInput="onInputEventHandler"
-        @onFocus="onFocusEventHandler"
-        @onBlur="onBlurEventHandler"
-    >
-    </vue3-simple-typeahead>
+  <section>
+    <o-field grouped>
+        <o-switch v-model="openOnFocus">Open dropdown on focus</o-switch>
+        <o-switch v-model="keepFirst">Keep-first</o-switch>
+    </o-field>
+
+    <o-field label="Find a name">
+        <o-autocomplete
+            v-model="userName"
+            placeholder="e.g. Anne"
+            :keep-first="keepFirst"
+            :open-on-focus="openOnFocus"
+            :data="filteredDataObj"
+            field="user.first_name"
+            @select="(option) => (selected = option)">
+            <template #empty> No results found </template>
+        </o-autocomplete>
+    </o-field>
+    <p><b>Selected:</b> {{ selected }}</p>
+  </section>
 </template>
--- Final-Project/client/src/main.ts
+++ Final-Project/client/src/main.ts
@@ -3,13 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import SimpleTypeahead from 'vue3-simple-typeahead';
-import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css';
+import Oruga from '@oruga-ui/oruga-next';
+import '@oruga-ui/theme-oruga/dist/scss/oruga-full.scss';
+
+
 
 const app = createApp(App)
 
 app
   .use(router)
-  .use(SimpleTypeahead)
+  .use(Oruga)
 
 app.mount('#app')
