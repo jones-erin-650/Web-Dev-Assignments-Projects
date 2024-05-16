709a556d731aeb6f3e86e2adf00073d5818e58c6
joneserin650 <joneserin650@gmail.com>
2024-05-16 12:54:21 -0400

    set up the typehead library

---
+++ Final-Project/client/src/components/TypeHead.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <vue3-simple-typeahead
+        id="typeahead_id"
+        placeholder="Search User Activities"
+        :items="['One','Two','Three']"
+        :minInputLength="1"
+        :itemProjection="itemProjectionFunction"
+        @selectItem="selectItemEventHandler"
+        @onInput="onInputEventHandler"
+        @onFocus="onFocusEventHandler"
+        @onBlur="onBlurEventHandler"
+    >
+    </vue3-simple-typeahead>
+</template>
--- Final-Project/client/src/main.ts
+++ Final-Project/client/src/main.ts
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import SimpleTypeahead from 'vue3-simple-typeahead';
+import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css';
 
 const app = createApp(App)
 
-app.use(router)
+app
+  .use(router)
+  .use(SimpleTypeahead)
 
 app.mount('#app')
